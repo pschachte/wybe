519d6cfbb6deb07a596ece9e3c1d076920e85528
schachte <>
2008-03-12 18:55:56 +0000

    Update lexer to be able to return 1 token at a time.

--- lex.pl
+++ lex.pl
@@ -1,5 +1,5 @@
 %  File     : lex.pl
-%  RCS      : $Id: lex.pl,v 1.3 2007/04/10 08:15:14 schachte Exp $
+%  RCS      : $Id: lex.pl,v 1.4 2008/03/13 05:55:56 schachte Exp $
 %  Author   : Peter Schachte
 %  Origin   : Mon Apr  9 14:16:33 2007
 %  Purpose  : Lexical analysis for frege
@@ -8,7 +8,8 @@
 
 :- module(lex, [
 		lex_file/2,
-		lex_stream/2
+		lex_stream/2,
+	        get_token/2
    ]).
 
 lex_file(Filename, Tokens) :-
@@ -18,82 +19,78 @@ lex_file(Filename, Tokens) :-
 
 
 lex_stream(Stream, Tokens) :-
-	get0(Stream, Char),
+	get_code(Stream, Char),
 	lex_stream1(Char, Stream, Tokens).
 
 
+get_token(Stream, Token) :-
+	get_code(Stream, Char),
+	lex_token(Char, Stream, Token).
+
+
 lex_stream1(Char, Stream, Tokens) :-
-	(   lex_token(Char, Stream, Token, Char1)
+	(   lex_token(Char, Stream, Token)
 	->  Tokens = [Token|Tokens1],
+	    get_code(Stream, Char1),
 	    lex_stream1(Char1, Stream, Tokens1)
 	;   Tokens = []
 	).
 
 
-lex_token(-1, _, _, _) :- !, fail.	% fail at eof
-lex_token(0'#, Stream, Token, Char) :-
+lex_token(-1, _, _) :- !, fail.			% fail at eof
+lex_token(0'#, Stream, Token) :-
 	!,
-	get0(Char1),
+	get_code(Stream, Char1),
 	skip_line(Char1, Stream),
-	get0(Char2),
-	lex_token(Char2, Stream, Token, Char).
-lex_token(0'(, Stream, bracket(round,open), Char) :-
+	get_code(Stream, Char2),
+	lex_token(Char2, Stream, Token).
+lex_token(0'(, _, bracket(round,open)) :-
+	!.
+lex_token(0'), _, bracket(round,close)) :-
+	!.
+lex_token(0'[, _, bracket(square,open)) :-
+	!.
+lex_token(0'], _, bracket(square,close)) :-
+	!.
+lex_token(0'{, _, bracket(curly,open)) :-
+	!.
+lex_token(0'}, _, bracket(curly,close)) :-
+	!.
+lex_token(0'', Stream, string(Chars,single)) :-
 	!,
-	get0(Stream, Char).
-lex_token(0'), Stream, bracket(round,close), Char) :-
+	get_code(Stream, Char1),
+	read_string(Char1, Stream, Chars, 0'').
+lex_token(0'", Stream, string(Chars,double)) :-
 	!,
-	get0(Stream, Char).
-lex_token(0'[, Stream, bracket(square,open), Char) :-
+	get_code(Stream, Char1),
+	read_string(Char1, Stream, Chars, 0'").
+lex_token(0'`, Stream, string(Chars,back)) :-
 	!,
-	get0(Stream, Char).
-lex_token(0'], Stream, bracket(square,close), Char) :-
+	get_code(Stream, Char1),
+	read_string(Char1, Stream, Chars, 0'`).
+lex_token(0'\\, Stream, string(Chars,here(Term))) :-
 	!,
-	get0(Stream, Char).
-lex_token(0'{, Stream, bracket(curly,open), Char) :-
-	!,
-	get0(Stream, Char).
-lex_token(0'}, Stream, bracket(curly,close), Char) :-
-	!,
-	get0(Stream, Char).
-lex_token(0'', Stream, string(Chars,single), Char) :-
-	!,
-	get0(Stream, Char1),
-	read_string(Char1, Stream, Chars, 0''),
-	get0(Stream, Char).
-lex_token(0'", Stream, string(Chars,double), Char) :-
-	!,
-	get0(Stream, Char1),
-	read_string(Char1, Stream, Chars, 0'"),
-	get0(Stream, Char).
-lex_token(0'`, Stream, string(Chars,back), Char) :-
-	!,
-	get0(Stream, Char1),
-	read_string(Char1, Stream, Chars, 0'`),
-	get0(Stream, Char).
-lex_token(0'\\, Stream, string(Chars,here(Term)), Char) :-
-	!,
-	get0(Stream, Char1),
+	get_code(Stream, Char1),
 	read_terminator(Char1, Stream, Term0),
 	Term = [0'\\|Term0],
-	read_here_string(Term, Stream, Chars, Seen, Seen, Term),
-	get0(Stream, Char).
-lex_token(Char0, Stream, Token, Char) :-
+	read_here_string(Term, Stream, Chars, Seen, Seen, Term).
+lex_token(Char0, Stream, Token) :-
 	Char0 =< 0' ,			% must be a whitespace character
 	!,
-	get0(Stream, Char1),
-	lex_token(Char1, Stream, Token, Char).
-lex_token(Char0, Stream, Token, Char) :-
+	get_code(Stream, Char1),
+	lex_token(Char1, Stream, Token).
+lex_token(Char0, Stream, Token) :-
 	symbol_char(Char0),
 	!,
 	Token = symbol([Char0|Chars]),
-	symbol_chars(Stream, Chars, Char).
-lex_token(Char0, Stream, punct([Char0|Chars]), Char) :-
-	nonsymbol_chars(Stream, Chars, Char).
+	symbol_chars(Stream, Chars).
+lex_token(Char0, Stream, punct([Char0|Chars])) :-
+	nonsymbol_chars(Stream, Chars).
 
 
 skip_line(0'\n, _) :- !.
 skip_line(_, Stream) :-
-	get0(Stream, Char),
+	get_code(Stream, Char),
 	skip_line(Char, Stream).
 
 
@@ -101,11 +98,11 @@ read_string(Char, Stream, Chars, Term) :-
 	(   Char =:= Term
 	->  Chars = []
 	;   Char =:= 0'\\
-	->  get0(Stream, Escaped),
+	->  get_code(Stream, Escaped),
 	    char_escape(Escaped, Stream, Chars, Chars1, Char1),
 	    read_string(Char1, Stream, Chars1, Term)
 	;   Chars = [Char|Chars1],
-	    get0(Char1),
+	    get_code(Char1),
 	    read_string(Char1, Stream, Chars1, Term)
 	).
 
@@ -113,11 +110,11 @@ read_string(Char, Stream, Chars, Term) :-
 char_escape(Char, Stream, Chars, Chars0, Char1) :-
 	(   one_char_escape(Char, Esc)
 	->  Chars = [Esc|Chars0],
-	    get0(Char1)
+	    get_code(Char1)
 	;   special_char_escape(Char, Stream, Chars, Chars0, Char1)
 	->  true
 	;   Chars = [Char|Chars0],
-	    get0(Char1)
+	    get_code(Char1)
 	).
 
 
@@ -137,7 +134,7 @@ one_char_escape(0'0, 0'\0).
 % backslash at end of line in a string ignores newline and all following
 % whitespace characters, so you can layout strings as you like.
 special_char_escape(0'\n, Stream, Chars, Chars, Char1) :-
-	get0(Char),
+	get_code(Char),
 	skip_white(Char, Stream, Char1).
 % XXX Also define an escape like \( ... ) to evaluate enclosed expr.
 % XXX This could handle formatted output pretty well.
@@ -148,7 +145,7 @@ skip_white(Char, Stream, Char1) :-
 	->  Char1 = Char
 	;   Char < 0
 	->  Char1 = Char
-	;   get0(Char2),
+	;   get_code(Char2),
 	    skip_white(Char2, Stream, Char1)
 	).
 
@@ -157,7 +154,7 @@ read_terminator(0'\\, _, Term) :-
 	!,
 	Term = [0'\\].
 read_terminator(Char, Stream, [Char|Term]) :-
-	get0(Stream, Char1),
+	get_code(Stream, Char1),
 	read_terminator(Char1, Stream, Term).
 
 
@@ -168,7 +165,7 @@ read_terminator(Char, Stream, [Char|Term]) :-
 
 read_here_string([], _, [], _, _, _).
 read_here_string([Char0|Term], Stream, Chars, Seen, Seen0, Wholeterm) :-
-	get0(Stream, Char),
+	get_code(Stream, Char),
 	(   Char =:= Char0
 	->  Seen0 = [Char|Seen1],
 	    read_here_string(Term, Stream, Chars, Seen, Seen1, Wholeterm)
@@ -195,23 +192,23 @@ punctuation_char(Char) :-
 	\+ symbol_char(Char).
 
 
-symbol_chars(Stream, Chars, Char1) :-
-	get0(Stream, Char),
+symbol_chars(Stream, Chars) :-
+	peek_code(Stream, Char),
 	(   symbol_char(Char)
 	->  Chars = [Char| Chars1],
-	    symbol_chars(Stream, Chars1, Char1)
-	;   Chars = [],
-	    Char1 = Char
+	    get_code(Stream, _),
+	    symbol_chars(Stream, Chars1)
+	;   Chars = []
 	).
 
 
-nonsymbol_chars(Stream, Chars, Char1) :-
-	get0(Stream, Char),
+nonsymbol_chars(Stream, Chars) :-
+	peek_code(Stream, Char),
 	(   punctuation_char(Char)
 	->  Chars = [Char| Chars1],
-	    nonsymbol_chars(Stream, Chars1, Char1)
-	;   Chars = [],
-	    Char1 = Char
+	    get_code(Stream, _),
+	    nonsymbol_chars(Stream, Chars1)
+	;   Chars = []
 	).
 
 special_char(0'().
