8a661d69f293bcda2f9025ca4a6707d90274720b
Peter Schachte <schachte@unimelb.edu.au>
2011-12-22 14:20:12 +1100

    Handle command line options, take source file name from command line

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 #  RCS      : $Id: Makefile,v 1.1 2003/03/30 13:43:53 schachte Exp $
 #  Author   : Peter Schachte
 
+VERSION=0.1
+
 all:	test
 
 %.pdf:	%.tex
@@ -13,8 +15,18 @@ all:	test
 %.hs:	%.y
 	happy $<
 
-frgc:	frgc.hs Parser.hs Scanner.hs
+frgc:	frgc.hs Parser.hs Scanner.hs Version.hs
 	ghc --make $@
+
+.PHONY: Version.hs
+
+Version.hs:
+	@rm -f $@
+	@printf "{- Version.hs automatically generated:  DO NOT EDIT -}\n" > $@
+	@printf "\n" >> $@
+	@printf "module Version (version,buildDate) where\n" >> $@
+	@printf "version :: String\nversion = \"%s\"\n" "$(VERSION)" >> $@
+	@printf "buildDate :: String\nbuildDate = \"%s\"\n" "`date`" >> $@
 
 TESTCASES = $(wildcard test-cases/*.frg)
 
@@ -23,7 +35,7 @@ test:	frgc
 	    printf "%-40s ... " $$f ; \
 	    out=`echo "$$f" | sed 's/.frg$$/.out/'` ; \
 	    exp=`echo "$$f" | sed 's/.frg$$/.exp/'` ; \
-	    ./frgc < $$f > $$out ; \
+	    ./frgc -v $$f > $$out ; \
 	    diff -q $$exp $$out >/dev/null && echo "PASS" ; \
 	    diff -q $$exp $$out >/dev/null || echo "FAIL" ; \
 	done
--- frgc.hs
+++ frgc.hs
@@ -10,7 +10,61 @@ module Main where
 
 import Parser
 import Scanner
+import System.Environment
+import System.Console.GetOpt
+import Version
 
+data Options = Options
+ { optVerbosity   :: Int
+ , optShowVersion :: Bool
+ , optShowHelp    :: Bool
+ } deriving Show
+
+defaultOptions    = Options
+ { optVerbosity   = 0
+ , optShowVersion = False
+ , optShowHelp    = False
+ }
+
+options :: [OptDescr (Options -> Options)]
+options =
+ [ Option ['v'] ["verbose"]
+     (NoArg (\ opts -> opts { optVerbosity = 1 + optVerbosity opts }))
+     "verbose output on stderr"
+ , Option ['V'] ["version"]
+     (NoArg (\ opts -> opts { optShowVersion = True }))
+     "show version number"
+ , Option ['h'] ["help"]
+     (NoArg (\ opts -> opts { optShowHelp = True }))
+     "display this help text and exit"
+ ]
+
+
+header :: String
+header = "Usage: frgc [OPTION...] files..."
+
+compilerOpts :: [String] -> IO (Options, [String])
+compilerOpts argv = 
+  case getOpt Permute options argv of
+    (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+
+main :: IO ()
 main = do
-  toks <- inputTokens
-  print $ parse $ map frgtoken toks
+  argv <- getArgs
+  (opts,files) <- compilerOpts argv
+  if optShowHelp opts then
+    putStrLn $ usageInfo header options
+    else if optShowVersion opts then
+           putStrLn $ "frgc " ++ version ++ "\nBuilt " ++ buildDate
+         else processFiles opts files
+
+processFiles :: Options -> [String] -> IO ()
+processFiles opts [] = return ()
+processFiles opts (file:files) = do
+  toks <- fileTokens file
+  let parseTree = parse $ map frgtoken toks
+  if (optVerbosity opts) > 0 then
+    print parseTree
+    else return ()
+  processFiles opts files
