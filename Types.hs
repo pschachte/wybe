daa97fe30b52ca4f7c380185b02552546b708d86
Peter Schachte <schachte@unimelb.edu.au>
2012-01-18 09:30:59 +1100

    create source file for type checker

---
+++ Types.hs
@@ -0,0 +1,9 @@
+--  File     : Types.hs
+--  Author   : Peter Schachte
+--  Origin   : Sun Jan 15 16:00:18 2012
+--  Purpose  : Type checker/inferencer for Frege
+--  Copyright: © 2012 Peter Schachte.  All rights reserved.
+
+module Types ( 
+             ) where
+
