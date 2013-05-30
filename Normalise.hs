8bc4fc11b90bc528e974d4cc4e2cbe0413677aa0
Peter Schachte <schachte@unimelb.edu.au>
2013-05-30 23:20:35 +1000

    Completed (untested) def-use code

--- Normalise.hs
+++ Normalise.hs
@@ -593,39 +593,82 @@ normaliseExp (ForeignFn lang name exps) pos = do
 
 
 ----------------------------------------------------------------
+type DefUse = (Set VarName,Set VarName)
+
+noDefUse :: DefUse
+noDefUse = (Set.empty, Set.empty)
+
+sequentialDefUse :: DefUse -> DefUse -> DefUse
+sequentialDefUse (d1,u1) (d2,u2) =
+    (d1 `Set.union` (d2 `Set.difference` u1), 
+     u1 `Set.union` (u2 `Set.difference` d1))
+
+parallelDefUse :: DefUse -> DefUse -> DefUse
+parallelDefUse (d1,u1) (d2,u2) = (d1 `Set.union` d2, u1 `Set.union` u2)
+
+joinDefUse :: DefUse -> DefUse -> DefUse
+joinDefUse (d1,u1) (d2,u2) = (d1 `Set.intersection` d2, u1 `Set.union` u2)
+
+defVar :: VarName -> DefUse
+defVar var = (Set.singleton var, Set.empty)
+
+useVar :: VarName -> DefUse
+useVar var = (Set.empty, Set.singleton var)
+
 -- |Return the set of variables used as inputs to the given code, other
 --  than those in the supplied set of variables.
-codeDefsUses :: [Placed Stmt] -> (Set VarName,Set VarName)
-codeDefsUses stmts =
-  List.foldr combineDefUse initDefUse
-  $ List.map stmtDefsUses stmts
+pstmtsDefUse :: [Placed Stmt] -> DefUse
+pstmtsDefUse stmts =
+    List.foldl sequentialDefUse noDefUse $ List.map pstmtDefUse stmts
 
-combineDefUse :: (Set VarName,Set VarName) -> (Set VarName,Set VarName) ->
-                 (Set VarName,Set VarName)
-combineDefUse (d1,u1) (d2,u2) =
-  (d1 `Set.union` (d2 \\ u1), u1 `Set.union` (u2 \\ d1))
+pstmtDefUse :: Placed Stmt -> DefUse
+pstmtDefUse placed = stmtDefUse $ content placed
 
-initDefUse :: (Set VarName,Set VarName)
-initDefUse = (Set.empty, Set.empty)
+stmtDefUse :: Stmt -> DefUse
+stmtDefUse (ProcCall _ args) = pexpsDefUse args
+stmtDefUse (ForeignCall _ _ args) = pexpsDefUse args
+stmtDefUse (Cond exp thn els) =
+    pexpDefUse exp `sequentialDefUse`
+    (pstmtsDefUse thn `joinDefUse` pstmtsDefUse els)
+stmtDefUse (Loop loop) = pstmtsDefUse loop
+stmtDefUse (Guard exp val) = pexpDefUse exp
+stmtDefUse Nop = noDefUse
+stmtDefUse (For gen) = genDefUse gen
+stmtDefUse (BreakIf cond) = pexpDefUse cond
+stmtDefUse (NextIf cond) = pexpDefUse cond
 
-stmtDefsUses (ProcCall _ args) = expListDefsUses args
-stmtDefsUses (ForeignCall _ _ args) = expListDefsUses args
-stmtDefsUses (Cond exp thn els) =
-  let uses1         = expUses exp
-      (defs2a,uses2a) = stmtDefsUses thn
-      (defs2b,uses2b) = stmtDefsUses els
-      defs2 = defs2a `Set.intersection` defs2b
-      uses2 = uses2a `Set.union` uses2b
-  in  ((defs2 \\ uses1, uses1 `Set.union` uses2)
-stmtDefsUses (Loop loop) = stmtDefsUses loop
-stmtDefsUses (Guard exp val) = (Set.empty, expUses exp)
-stmtDefsUses (Nop:rest) = (Set.empty, Set.empty)
-stmtDefsUses (For gen) = genDefsUses gen
-stmtDefsUses (BreakIf cond) = (Set.empty, expUses exp)
-stmtDefsUses (NextIf cond) = (Set.empty, expUses exp)
+genDefUse (In var exp) = pexpDefUse exp `sequentialDefUse` defVar var
+genDefUse (InRange var exp updateOp inc limit) =
+    -- XXX This handles the initialisation expr in the wrong place;
+    --     it should be handled at the top of the loop
+    ((pexpDefUse exp `sequentialDefUse` pexpDefUse inc) `sequentialDefUse`
+     case limit of
+         Nothing -> noDefUse
+         Just (_,pexp) -> pexpDefUse pexp) 
+    `sequentialDefUse` defVar var
 
+pexpsDefUse :: [Placed Exp] -> DefUse
+pexpsDefUse exps = List.foldr parallelDefUse noDefUse $
+                     List.map pexpDefUse exps
 
+pexpDefUse :: Placed Exp -> DefUse
+pexpDefUse placed = expDefUse $ content placed
 
+expDefUse :: Exp -> DefUse
+expDefUse (IntValue a) = noDefUse
+expDefUse (FloatValue a) = noDefUse
+expDefUse (StringValue a) = noDefUse
+expDefUse (CharValue a) = noDefUse
+expDefUse (Var name dir) =
+    (if flowsOut dir then Set.singleton name else Set.empty,
+     if flowsIn dir then Set.singleton name else Set.empty)
+expDefUse (Where stmts exp) =
+    pstmtsDefUse stmts `sequentialDefUse` pexpDefUse exp
+expDefUse (CondExp cond thn els) =
+    pexpDefUse cond `sequentialDefUse` 
+    (pexpDefUse thn `joinDefUse` pexpDefUse els)
+expDefUse (Fncall _ exps) = pexpsDefUse exps
+expDefUse (ForeignFn _ _ exps) = pexpsDefUse exps
 ----------------------------------------------------------------
 
 
