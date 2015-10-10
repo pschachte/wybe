8ca03ad14bb7525f9814daadab8c65bdc3063ca7
rishi <ashutoshrishi92@gmail.com>
2015-10-11 03:17:26 +1100

    Added temp config for running ld

--- Config.hs
+++ Config.hs
@@ -8,7 +8,9 @@
 -- |Compiler configuration parameters.  These may vary between
 --  OSes.
 module Config (sourceExtension, objectExtension, executableExtension,
-               interfaceExtension) where
+               interfaceExtension, bitcodeExtension, sharedLibs,
+                                 linkerArgs)
+    where
 
 -- |The file extension for source files.
 sourceExtension :: String
@@ -26,3 +28,23 @@ executableExtension = ""
 interfaceExtension :: String
 interfaceExtension = "int"
 
+-- |The file extension for bitcode files
+bitcodeExtension :: String
+bitcodeExtension = "bc"
+
+-- |Foreign shared library directory name
+sharedLibDirName :: String
+sharedLibDirName = "lib/"
+
+sharedLibs :: [FilePath]
+sharedLibs = ["cbits.so"]
+
+linkerArgs :: [String]
+linkerArgs =
+    ["-demangle", "-dynamic", "-arch", "x86_64",
+     "-macosx_version_min", "10.11.0",
+     "-syslibroot",
+     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk",
+     "-lSystem",
+     "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/7.0.0/lib/darwin/libclang_rt.osx.a"
+    ]
