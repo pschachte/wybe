542c0fe3c4c8a89d1c28f93fbc5efcacf1bfb1e6
rishi <ashutoshrishi92@gmail.com>
2015-11-01 16:52:48 +1100

    Added insertion & extraction of text into obj file
    Any text file content can be added into __LPVM segment, and extracted
    as string. The actual extracted content is represented as hex and
    relocation addresses, which is converted to human readable string.

--- Emit.hs
+++ Emit.hs
@@ -2,16 +2,18 @@ module Emit where
 
 import           AST
 import           Codegen
-import Data.Map as Map
 import           Control.Monad
-import           Control.Monad.Trans          (lift, liftIO)
+import           Control.Monad.Trans (lift, liftIO)
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.State
-import           Foreign.Ptr                  (FunPtr, castFunPtr)
-import qualified LLVM.General.AST             as LLVMAST
+import           Data.Hex
+import           Data.List as List
+import           Data.Map as Map
+import           Foreign.Ptr (FunPtr, castFunPtr)
+import qualified LLVM.General.AST as LLVMAST
 import           LLVM.General.CodeModel
 import           LLVM.General.Context
-import           LLVM.General.Module          as Mod
+import           LLVM.General.Module as Mod
 import           LLVM.General.Target
 
 import           LLVM.General.Analysis
@@ -23,8 +25,8 @@ import qualified LLVM.General.ExecutionEngine as EE
 import           System.Directory
 import           System.Process
 
-import           Config                       (linkerArgs, sharedLibs)
-import           Options                      (LogSelection (Emit))
+import           Config
+import           Options (LogSelection (Emit))
 
 foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> (IO Double)
 
@@ -177,16 +179,63 @@ runJIT mod = do
           return optmod
 
 
--- | With the `ld` linker, link the object files and create target.
+----------------------------------------------------------------------------
+-- -- * Linking                                                           --
+----------------------------------------------------------------------------
+
+
+-- | With the `ld` linker, link the object files and create target
+-- executable.
 makeExec :: [FilePath]          -- Object Files
          -> FilePath            -- Target File
-         -> IO ()               
+         -> IO ()
 makeExec ofiles target =
     do dir <- getCurrentDirectory
-       let args = ofiles ++ sharedLibs ++ linkerArgs
-                  ++ ["-o", target]       
+       let args = ofiles ++ sharedLibs ++ ldArgs ++ ldSystemArgs
+                  ++ ["-o", target]
        createProcess (proc "ld" args)
        return ()
+
+----------------------------------------------------------------------------
+-- -- * Object file manipulation                                          --
+----------------------------------------------------------------------------
+
+-- | Insert string data from first file into the second object file, into
+-- the segment '__LPVM', and section '__lpvm' using ld.
+insertLPVMData :: FilePath -> FilePath -> IO ()
+insertLPVMData datfile obj =
+    do let args = ["-r"] ++ ldSystemArgs
+                  ++ ["-sectcreate", "__LPVM", "__lpvm", datfile]
+                  ++ ["-o", obj]
+       createProcess (proc "ld" args)
+       return ()
+
+-- | Extract string data from the segment __LPVM, section __lpvm of the
+-- given object file. An empty string is returned if there is no data
+-- in that section. The program 'otool' is used to read the object file.
+extractLPVMData :: FilePath -> IO String
+extractLPVMData obj =
+    do let args = ["-s", "__LPVM", "__lpvm", obj]
+       sout <- readProcess "otool" args []
+       return $ parseSegmentData sout       
+
+-- | Parse the returned segment/section contents from it's HEX form to
+-- ASCII form.
+-- Sample:
+-- "test-cases/foo.o:\nContents of (__LPVM,__lpvm) section
+-- 0000000000000000    23 20 70 75 62 6c 69 63 20 66 75 6e 63 20 74 6f ...
+parseSegmentData :: String -> String
+parseSegmentData str = concat hexLines
+    where
+      tillHex = dropWhile (\c -> c /= '\t') -- Actual data after \t 
+      mappedLines = List.map tillHex (lines str)
+      filteredLines = List.filter (not . List.null) mappedLines
+      hexLines = List.map (hex2char . tail) filteredLines
+
+-- | Convert a string of 2 digit hex values to string of ascii characters.
+-- | Example: "23 20 70 75 62 6c 69 63" -> "# public"
+hex2char :: String -> String
+hex2char s = (concat . join) $ mapM unhex (words s)
 
 ----------------------------------------------------------------------------
 -- Logging                                                                --
