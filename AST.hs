d32aabcf561cb0353ae2434966fc848452a65dd7
Peter Schachte <schachte@unimelb.edu.au>
2016-01-29 18:42:38 +1100

    Improve FinalDump output to start each proc with a newline.  Also use the special name *main* for the main function, rather than the empty string.

--- AST.hs
+++ AST.hs
@@ -1859,7 +1859,9 @@ showProcDefs firstID (def:defs) =
 -- |How to show a proc definition.
 showProcDef :: Int -> ProcDef -> String
 showProcDef thisID procdef@(ProcDef n proto def pos _ _ vis inline sub) =
-    n ++ " > " ++ visibilityPrefix vis
+    "\n" 
+    ++ (if n == "" then "*main*" else n)
+    ++ " > " ++ visibilityPrefix vis
     ++ "(" ++ show (procCallCount procdef) ++ " calls)"
     ++ (if inline then " (inline)" else "")
     ++ showSuperProc sub
