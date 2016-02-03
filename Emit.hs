52e57ba17845a53111d4b028850bd13cf706f75f
ashutoshr <ashutoshrishi92@gmail.com>
2016-02-03 11:31:42 +1100

    Moved ObjectFile functions to new module

--- Emit.hs
+++ Emit.hs
@@ -7,7 +7,6 @@ import           Control.Monad
 import           Control.Monad.Trans (lift, liftIO)
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.State
-import           Data.Hex
 import           Data.List as List
 import           Data.Map as Map
 import           Foreign.Ptr (FunPtr, castFunPtr)
@@ -82,7 +81,7 @@ codeemit mod =
             do llstr <- moduleLLVMAssembly m
                -- putStrLn llstr
                return llstr
-     
+
 
 ----------------------------------------------------------------------------
 -- Target Emitters                                                        --
@@ -187,7 +186,7 @@ makeExec ofiles target =
        let args = ofiles ++ sharedLibs ++ ["-o", target]
        createProcess (proc "cc" args)
        return ()
-         
+
 -- makeExec ofiles target =
 --     do dir <- getCurrentDirectory
 --        let args = ofiles ++ sharedLibs ++ ldArgs ++ ldSystemArgs
@@ -195,46 +194,7 @@ makeExec ofiles target =
 --        createProcess (proc "ld" args)
 --        return ()
 
-----------------------------------------------------------------------------
--- -- * Object file manipulation                                          --
-----------------------------------------------------------------------------
 
--- | Insert string data from first file into the second object file, into
--- the segment '__LPVM', and section '__lpvm' using ld.
-insertLPVMData :: FilePath -> FilePath -> IO ()
-insertLPVMData datfile obj =
-    do let args = ["-r"] ++ ldSystemArgs
-                  ++ ["-sectcreate", "__LPVM", "__lpvm", datfile]
-                  ++ ["-o", obj]
-       createProcess (proc "ld" args)
-       return ()
-
--- | Extract string data from the segment __LPVM, section __lpvm of the
--- given object file. An empty string is returned if there is no data
--- in that section. The program 'otool' is used to read the object file.
-extractLPVMData :: FilePath -> IO String
-extractLPVMData obj =
-    do let args = ["-s", "__LPVM", "__lpvm", obj]
-       sout <- readProcess "otool" args []
-       return $ parseSegmentData sout       
-
--- | Parse the returned segment/section contents from it's HEX form to
--- ASCII form.
--- Sample:
--- "test-cases/foo.o:\nContents of (__LPVM,__lpvm) section
--- 0000000000000000    23 20 70 75 62 6c 69 63 20 66 75 6e 63 20 74 6f ...
-parseSegmentData :: String -> String
-parseSegmentData str = concat hexLines
-    where
-      tillHex = dropWhile (\c -> c /= '\t') -- Actual data after \t 
-      mappedLines = List.map tillHex (lines str)
-      filteredLines = List.filter (not . List.null) mappedLines
-      hexLines = List.map (hex2char . tail) filteredLines
-
--- | Convert a string of 2 digit hex values to string of ascii characters.
--- | Example: "23 20 70 75 62 6c 69 63" -> "# public"
-hex2char :: String -> String
-hex2char s = (concat . join) $ mapM unhex (words s)
 
 ----------------------------------------------------------------------------
 -- Logging                                                                --
@@ -245,7 +205,7 @@ logEmit s = logMsg Emit s
 
 -- | Log LLVM IR representation of the given module.
 logLLVMString :: ModSpec -> Compiler ()
-logLLVMString thisMod = 
+logLLVMString thisMod =
   do reenterModule thisMod
      maybeLLMod <- getModuleImplementationField modLLVM
      case maybeLLMod of
@@ -259,7 +219,7 @@ logLLVMString thisMod =
 -- | Pull the LLVMAST representation of the module and generate the LLVM
 -- IR String for it, if it exists.
 extractLLVM :: AST.Module -> Compiler String
-extractLLVM thisMod =  
+extractLLVM thisMod =
   do case (modImplementation thisMod) >>= modLLVM of
        Just llmod -> liftIO $ codeemit llmod
        Nothing -> return "No LLVM IR generated."
