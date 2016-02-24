bbb79674e2f574042b94e8ddbc088d369b19b4fa
ashutoshr <ashutoshrishi92@gmail.com>
2016-02-24 15:28:29 +1100

    Added ipow and isqrt C functions

--- cbits.c
+++ cbits.c
@@ -1,26 +1,27 @@
 /* cbits
-$ clang -fPIC -shared cbits.c -o cbits.so
+   $ clang -fPIC -shared cbits.c -o cbits.so
 */
 
 #include <stdio.h>
 #include <assert.h>
 #include <gc/gc.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // putchard - putchar that takes a double and returns 0.
 void print_int(int X) {
-  printf("%d", X);
-  fflush(stdout);
+    printf("%d", X);
+    fflush(stdout);
 }
 
 void print_float(double X) {
-  printf("%f", X);
-  fflush(stdout);
+    printf("%f", X);
+    fflush(stdout);
 }
 
 void print_string(const char *s) {
-  puts(s);
-  fflush(stdout);
+    puts(s);
+    fflush(stdout);
 }
 
 int read_char() {
@@ -28,6 +29,27 @@ int read_char() {
     ch = getchar();
     return ch;        
 }
+
+int ipow(int base, int exp)
+{
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
+
+int isqrt(int x) {
+    double s;
+    s = sqrt((double) x);
+    return (int)s;
+}
+
 
 // Boehm GC
 void *wybe_malloc(int size) {
