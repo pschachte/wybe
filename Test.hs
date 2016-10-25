a5efe16b16aacbe4b8578a00f302a21ed50361ea
ashutoshr <ashutoshrishi92@gmail.com>
2016-10-25 14:09:13 +1100

    Added Hspec compiler output behaviour testing

--- Test.hs
+++ Test.hs
@@ -3,26 +3,36 @@ module Test where
 import qualified Data.List as List
 import           NewParser (parseWybe)
 import           Parser (parse)
-import AST (Item)
 import           Scanner (fileTokens, Token)
-import           Test.Hspec
 import           System.Directory (listDirectory)
-import System.FilePath (takeExtension, combine)
-
+import           System.FilePath (takeExtension, combine, replaceExtension)
+import           System.IO (openFile, IOMode(..))
+import           System.Process
+import           Test.Hspec
+import           Test.Hspec.Core.Runner (hspecResult)
 
 
 
 main :: IO ()
-main = do
+main = runCompilerTest
+
+
+-----------------------------------------------------------------------------
+-- Parser Test                                                             --
+-----------------------------------------------------------------------------
+
+runParserTest :: IO ()
+runParserTest = do
     stoks <- wybeSourceTokens
-    hspec $ parserTestSpec stoks
+    _ <- hspecResult $ parserTestSpec stoks
+    return ()
 
 
 parserTestSpec :: [(FilePath, [Token])] -> Spec
-parserTestSpec fs = do
+parserTestSpec fs =
     describe "Parsec Parser" $
-        mapM_ (\(f, toks) -> do
-                     it ("build parse tree for " ++ f) $ do
+        mapM_ (\(f, toks) ->
+                     it ("builds parse tree for " ++ f) $
                          case parseWybe toks f of
                              Left err ->
                                  expectationFailure (show err)
@@ -31,18 +41,77 @@ parserTestSpec fs = do
               ) fs
 
 
+runCompilerTest :: IO ()
+runCompilerTest = do
+    fs <- wybeSourceFiles
+    _ <- hspecResult $ compilerTestSpec fs
+    return ()
+
+
+
+-----------------------------------------------------------------------------
+-- Compiler Output Test                                                    --
+-----------------------------------------------------------------------------
+
+-- | Specification for testing expected compiler output
+compilerTestSpec :: [FilePath] -> Spec
+compilerTestSpec fs =
+    describe "Wybemk compiler" $
+        mapM_ (\f -> do
+                      runIO (compileFile f)
+                      let expFile = expectedFileFor f
+                      let outFile = outputFileFor f
+                      expected <- runIO (readFile expFile)
+                      output <- runIO (readFile outFile)
+                      it ("compiles " ++ f ++
+                          " and returns matching expected output.") $
+                          expected `shouldBe` output
+              ) fs
+
+
+compileFile :: FilePath -> IO ()
+compileFile f = do
+    let exec = "gtimeout" --log=FinalDump --force-all"
+    let ofile = replaceExtension f ".o"
+    let args = [ "2", "./wybemk", "--log=FinalDump", "--force-all", ofile ]
+    -- outHandle <- openFile (outputFileFor f) WriteMode
+    -- logHandle <- openFile (logFileFor f) WriteMode 
+    -- let process = proc exec args
+    (_, sout, serr) <- readProcessWithExitCode exec args []
+    putStrLn $ "wybemk: compiling " ++ f
+    writeFile (outputFileFor f) sout
+    writeFile (logFileFor f) serr
+
+
+
+-----------------------------------------------------------------------------
+-- Helper Functions                                                        --
+-----------------------------------------------------------------------------
+
+
+-- | Tokenize wybe source files in the 'test-cases' directory.
 wybeSourceTokens :: IO [(FilePath, [Token])]
 wybeSourceTokens = do
     files <- wybeSourceFiles
     -- FilePath -> IO (FilePath, [Token])
     mapM (\f -> (,) <$> return f <*> fileTokens f) files
-    
 
-
+-- | All source files in the 'test-cases' dir, dynamically collected.
 wybeSourceFiles :: IO [FilePath]
 wybeSourceFiles = do
     let isWybe = List.filter ((== ".wybe") . takeExtension)
     let listDir d = List.map (combine d) <$> listDirectory d
     tests <- isWybe <$> listDir "test-cases"
-    others <- isWybe <$> listDir "wybelibs"
-    return $ others ++ tests
+    -- others <- isWybe <$> listDir "wybelibs"
+    return $ tests
+
+
+
+expectedFileFor :: FilePath -> FilePath
+expectedFileFor f = replaceExtension f ".exp"
+
+outputFileFor :: FilePath -> FilePath
+outputFileFor f = replaceExtension f ".out"
+
+logFileFor :: FilePath -> FilePath
+logFileFor f = replaceExtension f ".log"
