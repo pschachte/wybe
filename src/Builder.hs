dccfb14dc94c14c01735a87fffe0b4e84ae56fa3
Peter Schachte <schachte@unimelb.edu.au>
2019-08-19 16:23:47 +1000

    Update doc of build process.

--- src/Builder.hs
+++ src/Builder.hs
@@ -42,27 +42,112 @@
 --  traversals for analysis and compilation will more than make up
 --  for the few times we need to recompile.
 --
---  Once the code and dependencies for all modules in an SCC are
---  read, flattened, and recorded in each of the modules, and all
---  dependent (but not mutually dependent) modules are fully
---  compiled, the build process makes these steps for each module
---  SCC:
+--  Ensuring that all compiler phases happen in the right order is
+--  subtle, particularly in the face of mutual module dependencies.
+--  Following are the ordering dependencies.
 --
---    1.  The type dependencies are topologically sorted and SCCs
---        identified, type representations are determined one SCC at a time,
---        and code is generated for all constructors, accessors,
---        mutators, and utilities (Types)
---    2.  Resource imports and exports are checked (Resources)
---    3.  The types of exported procs are validated (Types)
---    4.  Proc argument types and modes are checked, and called
---        procs are resolved (Types)
---    5.  Proc resources are checked and transformed to args (Resources)
---    6.  Branches, loops, and nondeterminism are transformed away (Unbranch)
---    7.  Call graph SCCs are identified and topologically sorted.
---        The following steps are performed on call graph SCC at a
---        time, buttom up.
---    8   Procs are compiled to clausal form (Clause)
---    9.  Procs are optimised (Optimise)
+--  * Types: the types a type depends on need to have been processed
+--  before the type itself, so that sizes are known. In the case of
+--  recursive or mutually recursive type dependencies, all types in
+--  the recursion must be pointers. Types are transformed into
+--  submodules, and constructors, deconstructors, accessors,
+--  mutators, and auxiliary type procedures (equality tests, plus
+--  eventually comparisons, printers, pretty printers, etc.) are all
+--  generated as procedures within those submodules.  Therefore,
+--  these must be handled as submodules are.
+--
+--  * Resources:  the resources a resource depends on must have been
+--  processed before the resource itself.  (We currently don't
+--  support defining resources in terms of others, but the plan is
+--  to support that.)  The types in the module that defines a
+--  resource, and all module dependencies, must have been processed
+--  at least enough to know they have been defined to process the
+--  resource declaration.
+--
+--  * Top-level statements in a module:  these are transformed to
+--  statements in a special procedure whose name is the empty string
+--  as the statements are read, so their dependencies are the same
+--  as for statements in ordinary procedure bodies.
+--
+--  * Functions: functions and function calls are transformed to
+--  procedures and procedure calls without reference to anything
+--  external to the functions themselves, so function dependencies
+--  behave exactly like procedure dependencies.
+--
+--  * Procedures: the procedures a procedure calls must have been
+--  type and mode checked before they can themselves be type/mode
+--  checked, and must be analysed and optimised before they can
+--  themselves be analysed/optimised. All the procedures in the
+--  (sub)modules that define each type a procedure uses, as either a
+--  parameter or local variable type, must have been processed the
+--  same way before processing the procedure itself.
+--
+--  * Submodules:  the submodules of a module, including the types,
+--  must be processed as mutual dependencies of that module, as they
+--  are.  The nested submodules of a module (including types) have
+--  access to all public and private members of the parent module,
+--  and the parent has access to all public members of the parent,
+--  so they are mutual dependencies.
+--
+--  This means only minimal processing can be done before module
+--  dependencies are noted and read in.  So we handle all these
+--  dependencies by initially reading a module to be compiled and
+--  handling contents as follows:
+--
+--  * Types:  create and enter the submodule, note that parent
+--  imports it, and process its constructors and other contents.
+--
+--  * Submodules:  create and enter the submodule, note that parent
+--  imports it, and process its contents.
+--
+--  * Resources:  Record for later processing.
+--
+--  * Pragmas:  Record for later processing.
+--
+--  * Constructors: record for later type layout, code generation,
+--  etc.
+--
+--  * Top level statements:  add statements to the special "" proc
+--  in the module, creating it if necessary.
+--
+--  * Procs and functions:  record them for later normalisation,
+--  analysis, optimisation, etc.
+--
+--  Once we reach the end of a module or submodule, we call
+--  exitModule, which returns a list of modules that form an SCC in
+--  the module dependency graph.  That list is passed to
+--  compileModSCC, which does the following:
+--
+--    1. Traverse all recorded type submodules in the module list
+--       finding all type dependencies; topologically sort them and
+--       identify SCCs. For each SCC: determine type representation
+--       for all constructors, and generate and normalise all
+--       constructor, accessor, mutator, and utility functions.
+--       Record the primitive representation of the type. This is
+--       handled in the Normalise module.
+--
+--    2. Check all resource imports and exports. (Resources)
+--
+--    3. Normalise all recorded procs in their modules, including
+--       generated constructors, etc. (Normalise)
+--
+--    4. Validate the types of exported procs. (Types)
+--
+--    5. Check proc argument types and modes are checked, and
+--       resolve called procs. (Types)
+--
+--    6. Check proc resources and transform them to args.
+--      (Resources)
+--
+--    7. Transform away branches, loops, and nondeterminism.
+--       (Unbranch)
+--
+--    8. Topologically sort proc call graph and identify SCCs.  For
+--       each SCC, bottom-up, do the following:
+--
+--         1. Compile procs to clausal form (Clause)
+--
+--         2. Optimise procs (Optimise)
 --
 --  END MAJOR DOC
 
