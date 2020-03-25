d0128c13b320ad1d6959a4c62b6a3b2266d41387
Zijun Chen <c.zijunchen@gmail.com>
2020-03-25 11:01:31 +0800

    clean up.

--- src/Util.hs
+++ src/Util.hs
@@ -5,14 +5,12 @@
 --  License  : Licensed under terms of the MIT license.  See the file
 --           : LICENSE in the root directory of this project.
 
-{-# LANGUAGE DeriveGeneric #-}
-
-module Util (sameLength, maybeNth, checkMaybe, setMapInsert,
-             applyPair, fillLines, nop, sccElts, DisjointSet, 
+module Util (sameLength, maybeNth, setMapInsert,
+             fillLines, nop, sccElts, DisjointSet, 
              emptyDS, addOneToDS, unionTwoInDS, combineTwoDS, 
              removeOneFromDS, removeFromDS, connectedToOthersInDS,
-             mapDS, filterDS, dsToTransitivePairs, removeDupTuples,
-             pruneTuples, transitiveTuples, cartProd) where
+             mapDS, filterDS, dsToTransitivePairs,
+             cartProd) where
 
 
 import           Data.Graph
@@ -40,12 +38,6 @@ maybeNth n (_:es)
  | otherwise      = Nothing
 
 
--- |Test the value in a maybe, and if it fails, return Nothing.
-checkMaybe :: (a -> Bool) -> Maybe a -> Maybe a
-checkMaybe test Nothing = Nothing
-checkMaybe test (Just val) = if test val then Just val else Nothing
-
-
 -- |Insert an element into the set mapped to by the specified key in
 --  the given map.  Maps to a singleton set if there is no current
 --  mapping for the specified key.
@@ -56,11 +48,6 @@ setMapInsert key item dict =
                     Just s -> Just $ Set.insert item s)
     key dict
 
-
-
--- |Apply a pair of functions to a value and return a pair of results.
-applyPair :: (a->b,a->c) -> a -> (b,c)
-applyPair (f,g) val = (f val, g val)
 
 
 -- |fillLines marginText currColumn lineLength text
@@ -110,41 +97,6 @@ _normaliseTuple t@(x,y)
     | y < x    = (y,x)
     | otherwise = t
 
--- Helper: then remove duplicated alias pairs
-removeDupTuples :: Ord a => [(a,a)] -> [(a,a)]
-removeDupTuples =
-    List.map List.head . List.group . List.sort . List.map _normaliseTuple
-
--- Helper: prune list of tuples with int larger than the range
-pruneTuples :: Ord a => [(a,a)] -> a -> [(a,a)]
-pruneTuples tuples upperBound =
-    List.foldr (\(t1, t2) tps ->
-                if t1 < upperBound && t2 < upperBound then (t1, t2):tps
-                else tps) [] tuples
-
-
--- Helper: to expand alias pairs
--- e.g. Aliases [(1,2),(2,3),(3,4)] is expanded to
--- [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
--- items in pairs are sorted already
-transitiveTuples :: [(Int,Int)] -> [(Int,Int)]
-transitiveTuples [] = []
-transitiveTuples pairs =
-    let loBound = List.foldr (\(p1,p2) bound ->
-            if p1 < bound then p1
-            else bound) 0 pairs
-        upBound = List.foldr (\(p1,p2) bound ->
-            if p2 > bound then p2
-            else bound) 0 pairs
-        adj = buildG (loBound, upBound) pairs
-        undirectedAdj = buildG (loBound, upBound) (edges adj ++ _reverseE adj)
-        elements = vertices undirectedAdj
-    in List.foldr (\vertex tuples ->
-        let reaches = reachable undirectedAdj vertex
-            vertexPairs = [(vertex, r) | r <- reaches, r /= vertex]
-        in vertexPairs ++ tuples
-        ) [] elements
-
 
 -- Helper: Cartesian product of escaped FlowIn vars to proc output
 cartProd :: [a] -> [a] -> [(a, a)]
@@ -165,13 +117,13 @@ type DisjointSet a = Set (Set a)
 emptyDS = Set.empty
 
 _findFristInSet :: (a -> Bool) -> Set a -> Maybe a
-_findFristInSet f set = 
+_findFristInSet f = 
     Set.foldr 
         (\x result -> 
             case result of
-            Just _ -> result
-            Nothing -> if f x then Just x else Nothing
-            ) Nothing set
+                Just _ -> result
+                Nothing -> if f x then Just x else Nothing
+            ) Nothing
 
 addOneToDS :: Ord a => a -> DisjointSet a -> DisjointSet a
 addOneToDS x ds = 
@@ -188,25 +140,24 @@ unionTwoInDS x y ds =
         then ds
         else 
             let (ds', newSet') = case xSet of 
-                                Nothing -> (ds, Set.singleton x)
-                                Just xSet -> ((Set.delete xSet ds), xSet)
+                    Nothing -> (ds, Set.singleton x)
+                    Just xSet -> ((Set.delete xSet ds), xSet)
             in
             let (ds'', newSet'') = case ySet of
-                                Nothing -> (ds', Set.insert y newSet')
-                                Just ySet -> ((Set.delete ySet ds'), Set.union newSet' ySet)
+                    Nothing -> (ds', Set.insert y newSet')
+                    Just ySet -> ((Set.delete ySet ds'), Set.union newSet' ySet)
             in
                 Set.insert newSet'' ds''
 
 combineTwoDS :: Ord a => DisjointSet a -> DisjointSet a -> DisjointSet a
-combineTwoDS ds1 ds2 =
+combineTwoDS =
     Set.foldr
         (\singleSet resultDs ->
             case Set.toList singleSet of
                 [] -> resultDs -- can't be empty
                 x:xs -> 
                     let resultDs' = addOneToDS x resultDs in
-                    List.foldl (\ds y -> unionTwoInDS x y ds) resultDs' xs
-            ) ds1 ds2 
+                    List.foldl (\ds y -> unionTwoInDS x y ds) resultDs' xs)
 
 
 removeOneFromDS :: Ord a => a -> DisjointSet a -> DisjointSet a
@@ -221,8 +172,8 @@ removeOneFromDS x ds =
                     else  Set.insert xSet' ds'
 
 removeFromDS :: Ord a => Set a -> DisjointSet a -> DisjointSet a
-removeFromDS set ds = 
-    filterDS (\x -> not $ Set.member x set) ds
+removeFromDS set = 
+    filterDS (\x -> not $ Set.member x set)
 
 
 connectedToOthersInDS :: Ord a => a -> DisjointSet a -> Bool
@@ -233,8 +184,8 @@ connectedToOthersInDS x ds =
 
 -- The map function must be a bijection, i.e. 1-to-1 mapping.
 mapDS :: Ord a => (a -> a) -> DisjointSet a -> DisjointSet a
-mapDS f ds = 
-    Set.map (Set.map f) ds
+mapDS f = 
+    Set.map (Set.map f)
 
 filterDS :: Ord a => (a -> Bool) -> DisjointSet a -> DisjointSet a
 filterDS f ds =
@@ -243,10 +194,10 @@ filterDS f ds =
 
 
 dsToTransitivePairs :: Ord a => DisjointSet a -> Set (a, a)
-dsToTransitivePairs ds =
+dsToTransitivePairs =
     Set.foldr
         (\singleSet result ->
             cartesianProduct singleSet singleSet
             |> Set.union result |> Set.filter (\(a, b) -> a < b)
-            ) Set.empty ds
+            ) Set.empty
 
