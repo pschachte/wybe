8a67fc145ae144735c3a9cad31cd27a5cd1c766a
Peter Schachte <schachte@unimelb.edu.au>
2020-07-25 15:59:01 +1000

    Improve loggign message

--- src/BodyBuilder.hs
+++ src/BodyBuilder.hs
@@ -455,7 +455,8 @@ argExpandedPrim call@(PrimCall pspec args) = do
     args' <- mapM expandArg args
     params <- lift $ primProtoParams <$> getProcPrimProto pspec
     unless (sameLength args' params) $
-        shouldnt $ "arguments don't match params in call " ++ show call
+        shouldnt $ "arguments in call " ++ show call
+                   ++ "don't match params " ++ show params
     args'' <- zipWithM (transformUnneededArg $ zip params args) params args'
     return $ PrimCall pspec args''
 argExpandedPrim (PrimForeign lang nm flags args) = do
