aa93d3dda52cfa9e75f97df29ef3488d6797421d
Peter Schachte <schachte@unimelb.edu.au>
2021-12-17 08:47:26 +1100

    Make sure  ... causes a uniqueness error.  Attach position of  construct to moves to save and restore resource values.  Add test for this.

--- src/Unique.hs
+++ src/Unique.hs
@@ -35,13 +35,18 @@ data UniquenessError = ReuseUnique {
 
 -- | Set used to check correctness of uniqueness of the program
 data UniquenessState = UniquenessState {
+    -- |The variables used so far in the current scope, with their types.
     uniquenessUsedMap :: VarDict,
-    uniquenessErrors  :: [UniquenessError]
+    -- |The uniqueness errors seen so far.
+    uniquenessErrors  :: [UniquenessError],
+    -- |The maximum determinism allowed in the current scope.  Assignment to
+    -- unique variables is not allowed outside a deterministic scope.
+    uniquenessDetism  :: Determinism
 } deriving Show
 
 
 -- | Return an initial state for uniqueness checker
-initUniquenessState :: UniquenessState
+initUniquenessState :: Determinism -> UniquenessState
 initUniquenessState = UniquenessState Map.empty []
 
 
@@ -54,6 +59,8 @@ uniquenessCheckProc def _ = do
     let detism = procDetism def
     let params = procProtoParams $ procProto def
     logUniqueness $ "with params: " ++ show params
+    -- XXX this only considers whether arguments are unique, ignoring direction
+    -- of flow.  When users can declare their own unique types, this won't work.
     someUnique <- elem (Just True) <$>
                   mapM (((tmUniqueness . typeModifiers . modInterface <$>) <$>)
                         <$> getLoadingModule)
@@ -63,38 +70,186 @@ uniquenessCheckProc def _ = do
             $ showProcName name ++ " with unique argument can fail") 
     case procImpln def of
         ProcDefSrc body -> do
-            let state = foldStmts (const . const) uniquenessCheckExp
-                        initUniquenessState body
-            logUniqueness $ "After checking body: " ++ show state
-            let state' = List.foldl (uniquenessCheckParam name pos) state params
-            logUniqueness $ "After checking params: " ++ show state'
-            errs <- filterM (typeIsUnique . errTypeSpec) (uniquenessErrors state')
-            mapM_ reportUniquenessError errs
+            state <- uniquenessCheckDef name pos detism body params
+            logUniqueness $ "After checking params: " ++ show state
+            mapM_ reportUniquenessError $ reverse $ uniquenessErrors state
             return def
         _ -> shouldnt $ "Uniqueness check of non-source proc def "
                         ++ show (procName def)
 
 
--- | Check correctness of uniqueness for an expression
-uniquenessCheckExp :: UniquenessState -> Exp -> OptPos -> UniquenessState
-uniquenessCheckExp state (Typed (Var name ParamIn flow) ty _) pos =
-    let (UniquenessState usedMap errs) = state
-    in if Map.member name usedMap
-    then state {uniquenessErrors = ReuseUnique name ty pos flow Nothing : errs}
-    else state {uniquenessUsedMap = Map.insert name ty usedMap}
-uniquenessCheckExp state (Typed (Var name ParamOut _) ty _) _ =
-    state {uniquenessUsedMap = Map.delete name (uniquenessUsedMap state)}
-uniquenessCheckExp state _ _ = state
+----------------------------------------------------------------
+--               The Uniqueness Checker Monad
+----------------------------------------------------------------
+
+-- |The Uniqueness monad is a state transformer monad carrying the 
+--  uniqueness state over the compiler monad.
+type Uniqueness = StateT UniquenessState Compiler
 
 
-uniquenessCheckParam :: ProcName -> OptPos -> UniquenessState -> Param
-                     -> UniquenessState
-uniquenessCheckParam name pos state (Param p ty flow flowType)
- |  flowsOut flow && Map.member p (uniquenessUsedMap state) =
-     -- error:  producing value as output after using it
-     state { uniquenessErrors = ReuseUnique p ty pos flowType (Just name)
-                                : uniquenessErrors state}
- |  otherwise = state
+uniquenessCheckDef :: ProcName -> OptPos -> Determinism -> [Placed Stmt]
+                   -> [Param] -> Compiler UniquenessState
+uniquenessCheckDef name pos detism body params =
+    execStateT 
+    (uniquenessCheckStmts body >> mapM_ (uniquenessCheckParam name pos) params)
+    $ initUniquenessState detism
+
+
+-- | Note a variable use, reporting an error if it has already been used.
+variableUsed :: VarName -> OptPos -> TypeSpec -> ArgFlowType -> Uniqueness ()
+variableUsed name pos ty flowType = do
+    alreadyUsed <- Map.member name <$> gets uniquenessUsedMap
+    when alreadyUsed $ uniquenessErr $ ReuseUnique name ty pos flowType Nothing
+    isUnique <- lift $ typeIsUnique ty
+    when isUnique
+     $ modify $ \st -> st {uniquenessUsedMap = Map.insert name ty
+                                              $ uniquenessUsedMap st }
+
+
+-- | Note a variable use, reporting an error if it has already been used.
+variableAssigned :: VarName -> Uniqueness ()
+variableAssigned name =
+    modify $ \st -> st {uniquenessUsedMap = Map.delete name
+                                            $ uniquenessUsedMap st }
+
+
+-- | Report a uniqueness error
+uniquenessErr :: UniquenessError -> Uniqueness ()
+uniquenessErr err = do
+    modify $ \st -> st { uniquenessErrors = err : uniquenessErrors st }
+
+
+withDetism :: Uniqueness a -> Determinism -> Uniqueness a
+withDetism a detism = do
+    oldDetism <- gets uniquenessDetism
+    modify $ \state -> state { uniquenessDetism = detism }
+    result <- a
+    modify $ \state -> state { uniquenessDetism = oldDetism }
+    return result
+
+
+joinUniqueness :: Uniqueness () -> Uniqueness () -> Uniqueness ()
+joinUniqueness first second = do
+    initial <- get
+    let start = initial { uniquenessErrors = [] }
+    put start
+    first
+    firstState <- get
+    put start
+    second
+    secondState <- get
+    let allErrs = uniquenessErrors secondState ++ uniquenessErrors firstState
+                  ++ uniquenessErrors initial
+    let joinUsed = uniquenessUsedMap firstState 
+                   `Map.intersection` uniquenessUsedMap secondState
+    put $ UniquenessState joinUsed allErrs $ uniquenessDetism initial
+
+
+-- | Uniqueness check a statement sequence
+uniquenessCheckStmts :: [Placed Stmt] -> Uniqueness ()
+uniquenessCheckStmts = mapM_ $ placedApply uniquenessCheckStmt
+
+
+-- | Uniqueness check a single statement
+uniquenessCheckStmt :: Stmt -> OptPos -> Uniqueness ()
+uniquenessCheckStmt (ProcCall _ _ _ _ _ args) pos =
+    mapM_ (defaultPlacedApply uniquenessCheckExp pos) args
+uniquenessCheckStmt (ForeignCall _ _ _ args) pos =
+    mapM_ (defaultPlacedApply uniquenessCheckExp pos) args
+uniquenessCheckStmt (Cond tst thn els _ _) pos =
+    (defaultPlacedApply uniquenessCheckStmt pos tst `withDetism` SemiDet
+     >> uniquenessCheckStmts thn)
+    `joinUniqueness` uniquenessCheckStmts els
+uniquenessCheckStmt (Case exp cases deflt) pos = do
+    defaultPlacedApply uniquenessCheckExp pos exp
+    uniquenessCheckCases uniquenessCheckStmts cases deflt
+uniquenessCheckStmt (And stmts) _ = uniquenessCheckStmts stmts
+uniquenessCheckStmt (Or [] _) pos = return ()
+uniquenessCheckStmt (Or [stmt] _) pos =
+    defaultPlacedApply uniquenessCheckStmt pos stmt
+uniquenessCheckStmt (Or (stmt:stmts) x) pos =
+    (defaultPlacedApply uniquenessCheckStmt pos stmt `withDetism` SemiDet)
+    `joinUniqueness` uniquenessCheckStmt (Or stmts x) pos
+uniquenessCheckStmt (Not negated) pos =
+    defaultPlacedApply uniquenessCheckStmt pos negated
+uniquenessCheckStmt (TestBool exp) pos = uniquenessCheckExp exp pos
+uniquenessCheckStmt Nop pos = return ()
+uniquenessCheckStmt Fail pos = return ()
+uniquenessCheckStmt (Loop body _) _ = uniquenessCheckStmts body
+uniquenessCheckStmt (UseResources _ _ body) _ = uniquenessCheckStmts body
+uniquenessCheckStmt (For generators body) pos = do
+    mapM_ ((\gen -> do
+            placedApply uniquenessCheckExp $ genExp gen
+            placedApply uniquenessCheckExp $ loopVar gen
+        ) . content) generators
+    uniquenessCheckStmts body
+uniquenessCheckStmt Break pos = return ()
+uniquenessCheckStmt Next pos = return ()
+
+
+-- | Uniqueness check the cases in a case statement.
+uniquenessCheckCases :: (a -> Uniqueness ()) -> [(Placed Exp,a)] -> Maybe a
+                     -> Uniqueness ()
+uniquenessCheckCases f [] deflt = maybe (return ()) f deflt
+uniquenessCheckCases f ((pexp, body):rest) deflt =
+    (placedApply uniquenessCheckExp pexp `withDetism` SemiDet >> f body)
+    `joinUniqueness` uniquenessCheckCases f rest deflt
+
+
+-- | Uniqueness check an expression (recursively). When we see a use of a unique
+-- variable, we add it to a dictionary (recording its type).  When we see a
+-- subsequent use, we report an error.  When we see an assignment of a variable,
+-- we remove it from the map, as that means we have not yet seen a use of that
+-- value.  For an in/out flow, we treat this as first a use and then an
+-- assignment.
+uniquenessCheckExp :: Exp -> OptPos -> Uniqueness ()
+uniquenessCheckExp (Typed (Var name ParamIn flowType) ty _) pos =
+    variableUsed name pos ty flowType
+uniquenessCheckExp (Typed (Var name ParamOut _) ty _) _ =
+    variableAssigned name
+uniquenessCheckExp (Typed (Var name ParamInOut flowType) ty _) pos = do
+    variableUsed name pos ty flowType
+    variableAssigned name
+uniquenessCheckExp (Typed exp _ _) pos = uniquenessCheckExp exp pos
+uniquenessCheckExp IntValue{} _ = return ()
+uniquenessCheckExp FloatValue{} _ = return ()
+uniquenessCheckExp CharValue{} _ = return ()
+uniquenessCheckExp StringValue{} _ = return ()
+uniquenessCheckExp var@Var{} _ =
+    shouldnt $ "Untyped variable " ++ show var ++ " in uniqueness checking"
+uniquenessCheckExp (Where stmts exp) pos = do
+    uniquenessCheckStmts stmts
+    defaultPlacedApply uniquenessCheckExp pos exp
+uniquenessCheckExp (DisjExp e1 e2) pos =
+    (defaultPlacedApply uniquenessCheckExp pos e1 `withDetism` SemiDet)
+    `joinUniqueness` defaultPlacedApply uniquenessCheckExp pos e2
+uniquenessCheckExp (CondExp stmt e1 e2) pos =
+    (placedApply uniquenessCheckStmt stmt `withDetism` SemiDet
+     >> placedApply uniquenessCheckExp e1)
+    `joinUniqueness` placedApply uniquenessCheckExp e2
+uniquenessCheckExp (Fncall _ _ exps) pos =
+    mapM_  (placedApply uniquenessCheckExp) exps
+uniquenessCheckExp (ForeignFn _ _ _ exps) pos =
+    mapM_  (placedApply uniquenessCheckExp) exps
+uniquenessCheckExp (CaseExp exp cases deflt) pos = do
+    placedApply uniquenessCheckExp exp
+    uniquenessCheckCases (placedApply uniquenessCheckExp) cases deflt
+
+
+-- -- | Fold over the cases in a case expression
+-- foldCaseExp :: (Placed Exp,Placed Exp) -> a
+-- foldCaseExp sfn efn val (pexp, pval) =
+--     placedApply (foldExp sfn efn (placedApply (foldExp sfn efn val) pexp)) pval
+
+
+-- | Uniqueness check a parameter (including used resources) following checking
+-- of the proc body.  This will catch unique outputs following use of the value.
+uniquenessCheckParam :: ProcName -> OptPos -> Param -> Uniqueness ()
+uniquenessCheckParam name pos (Param p ty flow flowType) = do
+    used <- Map.member p <$> gets uniquenessUsedMap
+    when  (flowsOut flow && used)
+     $ uniquenessErr $ ReuseUnique p ty pos flowType (Just name)
+
 
 -- | Check if a type is unique
 typeIsUnique :: TypeSpec -> Compiler Bool
