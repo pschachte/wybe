30beb2e80a0f2daa8ae471203d2b60e4ddaee0e7
James <41111926+jimbxb@users.noreply.github.com>
2022-03-25 15:57:10 +1100

    Fix for bug #250 (#258)
    * Fix for bug #250
    
    * replace Cond with Not

--- src/Types.hs
+++ src/Types.hs
@@ -2044,7 +2044,15 @@ modecheckStmt m name defPos assigned detism tmpCount final
       else if mustFail assigned1 -- is condition guaranteed to fail?
       then do
         logTyped $ "Assigned by failing conditional: " ++ show assigned3
-        return (tstStmt'++elsStmts', assigned3, tmpCount3)
+        bindings <- mapFromUnivSetM ultimateVarType Set.empty
+                    $ bindingVars assigned3
+        impurity <- lift $ stmtsImpurity tstStmt'
+        let stmts' = if impurity > Pure 
+                     -- if the test is non-pure, need to keep the test around
+                     then Not (seqToStmt tstStmt') `maybePlace` pos:elsStmts'
+                     -- otherwise, the cond must fail and wont bind anything
+                     else elsStmts'
+        return (stmts', assigned3, tmpCount3)
       else do
         let finalAssigned = assigned2 `joinState` assigned3
         logTyped $ "Assigned by conditional: " ++ show finalAssigned
@@ -2053,7 +2061,7 @@ modecheckStmt m name defPos assigned detism tmpCount final
         return
           ([maybePlace (Cond (seqToStmt tstStmt') thnStmts' elsStmts'
                         (Just condBindings) (Just bindings) res)
-            pos], finalAssigned,tmpCount)
+            pos], finalAssigned,tmpCount3)
 modecheckStmt m name defPos assigned detism tmpCount final
     stmt@(TestBool exp) pos = do
     logTyped $ "Mode checking test " ++ show exp
