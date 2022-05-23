00252f57c080357356a2e33cda4b04cd19245a84
Chris Chamberlain <chris@chamberlain.id.au>
2022-05-23 12:20:13 +1000

    further benchmark improvements

--- wybelibs/benchmark_impl.c
+++ wybelibs/benchmark_impl.c
@@ -7,21 +7,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct {
-    clock_t start;
-} benchmark_state_t;
-
-benchmark_state_t *benchmark_start() {
-    benchmark_state_t *benchmark_state = (benchmark_state_t*) malloc(sizeof(benchmark_state_t));
-    assert(benchmark_state);
-    benchmark_state->start = clock();
-    return benchmark_state;
-}
-
-double benchmark_finish(benchmark_state_t *benchmark_state) {
-    assert(benchmark_state);
-    clock_t end = clock();
-    double cpu_time_used = ((double) end - benchmark_state->start) / CLOCKS_PER_SEC;
-    free(benchmark_state);
-    return cpu_time_used;
+double get_clock_time() {
+    return ((double) clock()) / CLOCKS_PER_SEC;
 }
