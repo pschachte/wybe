18724ad36937b2e382e8f048fb66a08d51774246
James <41111926+jimbxb@users.noreply.github.com>
2022-07-11 13:03:36 +1000

    Fix for #342 (#343)
    * fix for #342
    
    * restricted saturated partial application recognition
    
    * reverted allowing resourceful expressions; added a test-case to be sure of it

--- src/Types.hs
+++ src/Types.hs
@@ -1058,12 +1058,14 @@ procToPartial :: [FlowDirection] -> Bool -> CallInfo -> (Maybe CallInfo, Bool)
 procToPartial callFlows hasBang info@FirstInfo{fiPartial=False,
                                                fiTypes=tys,
                                                fiFlows=flows,
+                                               fiInRes=inRes,
+                                               fiOutRes=outRes,
                                                fiNeedsResBang=resful,
                                                fiDetism=detism,
                                                fiImpurity=impurity}
-    | not hasBang  && not (List.null callFlows) && last callFlows == ParamOut
-                   && (length callFlows < length tys
-                       || length callFlows <= length tys + 1 && resful)
+    | not hasBang && not (List.null callFlows) && last callFlows == ParamOut
+                  && (length callFlows < length tys
+                      || length callFlows <= length tys + 1 && usesResources)
         = (Just info{fiPartial=True,
                      fiTypes=closedTys ++ [higherTy],
                      fiFlows=closedFls ++ [ParamOut]}, needsBang)
@@ -1071,6 +1073,7 @@ procToPartial callFlows hasBang info@FirstInfo{fiPartial=False,
     nClosed = length callFlows - 1
     (closedTys, higherTys) = List.splitAt nClosed tys
     (closedFls, higherFls) = List.splitAt nClosed flows
+    usesResources = not (Set.null inRes) || not (Set.null outRes)
     needsBang = resful || impurity > Pure
     higherTy = HigherOrderType (normaliseModifiers
                                 $ ProcModifiers detism MayInline
@@ -1612,8 +1615,7 @@ matchTypes caller callee pos _ callTypes callFlows
                         (typeFlowMode <$> callTFs) (typeFlowMode <$> tfs) [1..]
                 else typeError $ ReasonArity caller callee pos nCallTFs (length tfs')
             _ ->
-                void $ getTyping 
-                     $ unifyTypes (ReasonHigher caller callee pos)
+                void $ unifyTypes (ReasonHigher caller callee pos)
                         fnTy $ HigherOrderType defaultProcModifiers callTFs
     let typing' = snd typing
     let errs = typingErrs typing'
