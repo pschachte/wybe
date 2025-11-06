29e257490a86faaa0dded7e23235db0cfb997d3b
Peter Schachte <schachte@unimelb.edu.au>
2025-11-06 17:39:14 +1100

    Add Haskell, Rust, and 2 C versions of prime benchmarks

---
+++ benchmarks/primes/c_prime_backwards.c
@@ -0,0 +1,38 @@
+// Print the NTH prime number using the Sieve of Eratosthenes
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NTH 50000
+
+int sieve(int n) {
+    int *primes = malloc(n * sizeof(int));
+    int prime = 1;
+    // loop invariant:  primes[0..i-1] are the first i primes
+    for (int i = 0; i < n; i++) {
+        for (int candidate = prime+1;; candidate++) {
+            // check if candidate is prime
+            int is_prime = 1;
+            for (int j = i-1; j >= 0; j--) {
+                if (candidate % primes[j] == 0) {
+                    is_prime = 0;
+                    break;
+                }
+            }
+            if (is_prime) {
+                primes[i] = prime = candidate;
+                break;
+            }
+        } 
+    }
+    int result = primes[n-1];
+    free(primes);
+    return result;
+}
+
+
+int main(void) {
+    int n = NTH;
+    printf("%d\n", sieve(n));
+    return 0;
+}
