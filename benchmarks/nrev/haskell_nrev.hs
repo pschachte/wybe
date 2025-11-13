4c0693a093092111c28a918118dc2dbd145b8598
Peter Schachte <schachte@unimelb.edu.au>
2025-11-13 18:18:17 +1100

    Minor tweaks to 2 nrev tests

--- benchmarks/nrev/haskell_nrev.hs
+++ benchmarks/nrev/haskell_nrev.hs
@@ -3,18 +3,22 @@
 -- List type
 data Mylist t = Nil | Cons t (Mylist t)
 
+-- Append two lists
 append :: Mylist t -> Mylist t -> Mylist t
 append Nil ys = ys
 append (Cons x xs) ys = Cons x (append xs ys)
 
+-- Reverse a list
 nrev :: Mylist t -> Mylist t
 nrev Nil = Nil
 nrev (Cons x xs) = append (nrev xs) (Cons x Nil)
 
+-- Find the length of a list
 len :: Mylist t -> Int
 len Nil = 0
 len (Cons _ xs) = 1 + len xs
 
+-- Make a list of numbers from lo (inclusive) to hi (exclusive) 
 fromTo :: Int -> Int -> Mylist Int
 fromTo lo hi =
     if lo >= hi
@@ -23,5 +27,6 @@ fromTo lo hi =
 
 main :: IO ()
 main = do
-    let rev_size = len $ nrev $ fromTo 0 100000
+    let size = 100000
+    let rev_size = len $ nrev $ fromTo 0 size
     putStrLn $ "Reversed list size = " ++ show rev_size
