98158879fbe714df26ea1a9785c3ac338734bcdd
Peter Schachte <schachte@unimelb.edu.au>
2026-02-18 13:58:26 +1100

    Fix non-working fix for messed up /tmp directory on MacOS

--- test-cases/complex/utils.py
+++ test-cases/complex/utils.py
@@ -107,10 +107,10 @@ class Context:
         output = re.sub(r"@([A-Za-z_]\w*):[0-9:]*", r"@\1:nn:nn", output)
         output = re.sub(r"\[ [0-9][0-9]* x i8 \]", "[ ?? x i8 ]", output)
         output = re.sub(r"(target triple *= *).*", r"\1???", output)
-        # Hack around weird MacOS tmp dir
-        output = output.replace("/private/tmp/", "/tmp/")
         output = output.replace(self.tmp_dir, "!TMP!")
         output = output.replace(self.root_dir, "!ROOT!")
+        # Hack around weird MacOS tmp dir
+        output = output.replace("/private!TMP!", "!TMP!")
         return output
 
     def execute_program(self, exe: str, check: bool,
